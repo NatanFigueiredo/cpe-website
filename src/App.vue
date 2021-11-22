ba05e4f49b692055f2935069918d2f7f894cdfc7
Natan Figueiredo <natanfigueiredo27@gmail.com>
2021-11-22 19:21:22 -0300

    adding header and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+		<Header/>
     <router-view/>
+
+		<footer>
+			
+		</footer>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+	components: {
+		'Header': Header
+	}
+}
+
+</script>
 
 <style>
 #app {
@@ -17,16 +30,16 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.cpe-azul {
+	background-color: #12385B;
 }
 
-#nav a {
+.navbar-nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+.navbar-nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light cpe-azul">
+				
+				<div class="container">
+					<a class="navbar-brand" href="#">
+						<img src="../assets/logo-cpe.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+					</a>
+
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+						<div class="navbar-nav">
+							<router-link class="nav-link" to="/">Home</router-link>
+							<router-link class="nav-link" to="/about">About</router-link>
+						</div>
+					</div>
+				</div>
+
+			</nav>
+
+		</header>
+    
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
