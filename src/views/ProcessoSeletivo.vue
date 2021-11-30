cd269d963784fa076345a19b066c95db3e909744
Natan Figueiredo <natanfigueiredo27@gmail.com>
2021-11-30 14:07:05 -0300

    adding ps page

--- src/views/ProcessoSeletivo.vue
+++ src/views/ProcessoSeletivo.vue
@@ -1,9 +1,93 @@
 <template>
-	<Banner 
-		title="Processo Seletivo de Estudantes"
-		imageName="sala1.jpg"
-		imageAlt="Estudante em uma sala de aula"
-	/>
+	<div>
+		<Banner 
+			title="Seja estudante do CPE"
+			imageName="sala1.jpg"
+			imageAlt="Estudante em uma sala de aula"
+		/>
+
+		<div class="mt-4 container-fluid d-flex flex-column align-items-center">
+
+            <p class="h1 d-inline-flex title">Processo Seletivo de Estudantes</p>
+
+			<div class="d-flex flex-column align-items-center d-sm-flex flex-sm-row justify-content-sm-center m-2">
+				<img src="@/assets/Icons/selection.png" alt="Icone de uma pessoa sendo escolhida dentre três no total" class="img-fluid icon lg-icon">
+				<div class="w-50 w-sm-100 m-0">
+					<p class="mt-4">Para ser estudante do Cursinho, é necessario se inscrever e ser aprovado em nosso processo seletivo </p>
+					<p class="d-flex flex-column align-items-center">
+						Não se preocupe, ele é bem simples e tem como objetivo priorizar, mas não exclusivamente, determinadas pessoas para que estas tenham acesso a educação de qualidade <br>
+						No CPE, damos prioridades para pessoas nas seguintes condições:
+						<ul class="m-2 d-flex flex-column align-items-start">
+							<li>Estudantes de escola pública; 🏫 </li>
+							<li>Família de baixa renda; 👨‍👨‍👧‍👦</li>
+							<li>Moradores da Zona Leste de São Paulo; 🗺️</li>
+							<li>PPI; 👩🏾</li>
+							<li>Pessoas transsexuais, transgêneros ou travestis. 🏳️‍🌈</li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+			<div class="alert alert-danger" role="alert">
+				No momento não possuimos nenhum PS em andamento <br>
+				Siga nossas redes sociais para saber quando um novo processo seletivo de estudantes ocorrer
+			</div>
+            
+
+			<!-- <div class="divider-azul w-75 mx-auto"/>
+
+            <div class="d-flex align-items-center flex-column m-4 section">
+                <p class="h2 subtitle">Como funciona o PS de estudantes?</p>
+
+                <div class="d-flex flex-column d-sm-flex flex-sm-row justify-content-center">
+                    <div class="m-2 box">
+                        <img src="@/assets/Icons/sun.png" alt="Periodo vespertino" class="img-fluid icon">
+                        <p class="mt-4 h5">Vespertino</p>
+                        <p>Essa turma ocorre ao longo do período da tarde das 14:00 às 18:40</p>
+                    </div>
+                    <div class="m-2 box">
+                        <img src="@/assets/Icons/moon.png" alt="Periodo Noturno" class="img-fluid icon">
+                        <p class="mt-4 h5">Noturna</p>
+                        <p>
+                            Essa turma ocorre no período noturno, ideal para estudantes que trabalhem ou tenham outras atividades durante o dia <br> <br>
+                            As aulas ocorrem das 19:00 às 22:30
+                        </p>
+                    </div>
+                </div>
+            </div> -->
+
+			<div class="divider-azul w-75 mx-auto"/>
+
+			<div class="d-flex align-items-center flex-column m-4">
+				<p class="h2 subtitle">Procesos seletivos anteriores</p>
+				<p class="mt-2">Veja aqui os editais de processos seletivos anteriores</p>
+				<div id="processosanteriores" class="d-flex flex-column d-sm-flex flex-sm-row justify-content-center">
+					<div class="m-2">
+						<p class="mt-4 h5">Turma Extensiva</p>
+						<div class="d-flex flex-column">
+							<a class="m-2" href="http://bit.ly/ManualProcessoSeletivo20212 ">
+								<button type="button" class="btn btn-lg">2021/2</button>
+							</a>
+							<a class="m-2" href="http://bit.ly/ManualProcessoSeletivo20211">
+								<button type="button" class="btn btn-lg">2021/1</button>
+							</a>
+						</div>
+					</div>
+					<div class="m-2">
+						<p class="mt-4 h5">Turma de Revisão</p>
+						<div class="d-flex flex-column">
+							<a class="m-2" href="http://bit.ly/ManualProcessoSeletivo20212">
+								<button type="button" class="btn btn-lg">2020</button>
+							</a>
+
+						</div>
+					</div>
+				</div>
+				
+				
+			</div>
+        </div>
+	</div>
 </template>
 
 <script>
@@ -18,5 +102,19 @@ export default {
 </script>
 
 <style>
+
+@media (min-width: 576px) { 
+	.lg-icon {
+		margin: 50px;
+		min-width: 250px;
+	}
+}
+
+.btn {
+	background-color: #EF7A20;
+	color: white;
+	border: 1px solid #12385B;
+	width: 100%;
+}
 
 </style>
