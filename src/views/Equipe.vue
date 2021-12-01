bb92883b56ddea66e99f4da5b63800579d6a5f05
Natan Figueiredo <natanfigueiredo27@gmail.com>
2021-11-30 22:11:23 -0300

    adding team page

--- src/views/Equipe.vue
+++ src/views/Equipe.vue
@@ -5,6 +5,105 @@
 			imageName="equipe1.jpg"
 			imageAlt="Pessoas voluntárias sorrindo e segurando uma bandeira com o logo do cursinho "
 		/>
+
+		<div class="mt-2">
+			<p class="h1 d-inline-flex title">Acreditamos no poder transformador da educação</p>
+			<p>
+                Todas as pessoas do cursinho, docentes e membros administrativos, são pessoas voluntárias e unidas pelo propósito de nossos estudantes  <br>
+                Veja abaixo mais informações sobre as pessoas que foram o CPE
+            </p>
+		</div>
+
+		<div class="m-2 divider-azul w-75 mx-auto"/>
+
+		<!-- Administrativo  -->
+		<div id="adm" class="m-4 d-flex d-flex flex-column justify-content-center align-items-center">
+			<p class="h2 subtitle">Pessoas voluntárias administrativas</p>
+			<div class="content d-flex flex-column-reverse d-lg-flex flex-lg-row justify-content-center align-items-center w-100">	
+				<div class="textColumn">
+					<p> 
+						A parte administrativa do cursinho é dividida em departamentos que colaboram ativamente para fazer a melhor gestão e levar a melhor experiência de estudo para nossos alunos 
+						<br>
+						Em sua maioria, são estudantes de graduação e pós-graduação da EACH-USP que dedicam algumas horas semanais colaborando com o CPE
+					</p>
+				</div>
+				<div class="imageBox"/>
+			</div>
+
+			<div class="content d-flex flex-column d-lg-flex flex-lg-row justify-content-center align-items-center w-100">
+				<div class="imageBox"/>
+				<div class="textColumn d-flex justify-content-center">
+					<p>
+						O CPE fornece um ambiente de intensa colaboração além de
+						<ul class="d-flex flex-column align-items-start">
+							<li> Certificado de voluntariado </li>
+							<li> Horas de estágio (verifique os cursos)</li>
+						</ul>
+					</p>
+				</div>
+			</div>
+			
+			<!-- Alerts -->
+			<p class="mt-4 h5"> Tem interesse em fazer parte do corpo administrativo do Cursinho e realizar sonhos? </p>
+			<div v-if="psAdm" class="alert alert-success" role="alert">
+				Nosso processo seletivo para membros administrativos está aberto <br>
+				<a class="m-2" href="http://bit.ly/ManualProcessoSeletivo20212">
+					<button type="button" class="btn btn-sm mt-2">Se inscreva aqui</button>
+				</a>
+			</div>
+			<div v-else class="alert alert-primary" role="alert">
+				Preencha nosso fomulário de interresse e fique sabendo assim que o processo seletivo iniciar <br>
+				<a class="m-2" href="https://bit.ly/FormulárioInteresseCPE">
+					<button type="button" class="btn btn-sm mt-2">Forms de Interesse </button>
+				</a>
+			</div>
+
+		</div>
+
+		<div class="m-2 divider-azul w-75 mx-auto"/>
+
+		<!-- Professores -->
+		<div id="docentes" class="m-4 d-flex flex-column justify-content-center align-items-center">
+			<p class="h2 subtitle">Pessoas docentes e plantonistas</p>
+
+			<div class="content d-flex flex-column-reverse d-lg-flex flex-lg-row justify-content-center align-items-center w-100">	
+				<div class="textColumn">
+					<p> 
+						As aulas no cursinho são lecionadas por docentes que acreditam na educação e fazem disso sua paixão.
+						<br>
+						Temos professores em todas as areas de conhecimento de vestibulares, que desenvolvem materiais próprios e especificos para uso dentro do CPE
+					</p>
+				</div>
+				<div class="imageBox"/>
+			</div>
+
+			<div class="content d-flex flex-column d-lg-flex flex-lg-row justify-content-center align-items-center w-100">	
+				<div class="imageBox"/>
+				<div class="textColumn">
+					<p>
+						O CPE conta também com uma equipe de plantonistas voluntários, a disposição para auxliar e tirar dúvidas de estudantes ao longo do ano
+					</p>
+				</div>
+			</div>
+			
+			
+
+			<p class="mt-4 h5"> Tem interesse em lecionar no Cursinho e colaborar com a educação popular? </p>
+			<div v-if="psProf" class="alert alert-success" role="alert">
+				Nosso processo seletivo para professores e plantonistas está aberto <br>
+				<a class="m-2" href="http://bit.ly/ManualProcessoSeletivo20212">
+					<button type="button" class="btn btn-sm mt-2">Se inscreva aqui</button>
+				</a>
+			</div>
+			<div v-else class="alert alert-primary" role="alert">
+				Preencha nosso fomulário de interresse e fique sabendo assim que o processo seletivo iniciar <br>
+				<a class="m-2" href="https://bit.ly/FormulárioInteresseCPE">
+					<button type="button" class="btn btn-sm mt-2">Forms de Interesse </button>
+				</a>
+			</div>
+			
+
+		</div>
 	</div>
 </template>
 
@@ -12,13 +111,43 @@
 import Banner from '../components/Banner.vue'
 
 export default {
-		components: {
-				Banner
+	data() {
+		return {
+			psAdm: false,
+			psProf: false,
 		}
+	},
+	components: {
+			Banner
+	}
 
 }
 </script>
 
 <style>
+
+div .imageBox {
+	margin: 4px;
+	width: 200px;
+	height: 200px;
+	border: 1px solid red;
+	background-color: red;
+}
+
+@media (min-width: 576px) { 
+	div .textColumn {
+		width: 60%;
+	}
+	
+	div .content {
+		max-width: 75%;
+	}
+}
+
+@media (max-width: 576px) { 
+	.subtitle {
+		font-size: 125%;
+	}
+}
 
 </style>
