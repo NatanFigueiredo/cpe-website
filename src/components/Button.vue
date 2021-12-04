272a2a90bee58956969a7300c90d47af881a2ed1
Natan Figueiredo <natanfigueiredo27@gmail.com>
2021-12-04 17:06:01 -0300

    adding component button with wai-aria

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <a class="m-2" :href="link">
+        <button 
+            type="button" 
+            :class="cssClasses"
+            role="button"> 
+                {{content}}
+        </button>
+    </a>
+  
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: ['cssClasses', 'link', 'content']
+}
+</script>
+
+<style>
+
+.btn {
+	background-color: #EF7A20;
+	color: white;
+	border: 1px solid #12385B;
+	width: 50%;
+}
+
+</style>
